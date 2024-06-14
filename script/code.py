aae03f854f578f68ad1f3413f3e75d61923f0d8a
3r1ckZ <77485725+3r1ckZ@users.noreply.github.com>
2024-06-14 17:42:32 +0800

    Update code.py
    save result to text files

--- script/code.py
+++ script/code.py
@@ -11,11 +11,14 @@ image = 'moyu.png'
 f = open(image, 'rb')
 f_str = base64.b64encode(f.read())
 f.close()
-print(f_str)
-print(type(f_str))
+f1 = open('2.txt', 'wb')
+f1.write(f_str)
+f1.close()
 
 # 将二进制字符串（图片）decode为图片
-str = f_str
+# str = f_str
+f2 = open("2.txt", 'rb')
+str = f2.read()
 file_str = open('2.png', 'wb')
 file_str.write(base64.b64decode(str))
 file_str.close()
